cdcdf7a792e112eeb17add684b3ea6526fbc3a73
Tiago Bruno Giaquinto <diretor@giartechsolucoes.com.br>
2025-10-31 08:22:20 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/UserContext'
 import WebLayout from './components/layouts/WebLayout'
 import WebDashboard from './components/web/WebDashboard'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <UserProvider>
-        <WebLayout>
-          <WebDashboard />
-        </WebLayout>
-      </UserProvider>
-    </BrowserRouter>
+    <UserProvider>
+      <WebLayout>
+        <WebDashboard />
+      </WebLayout>
+    </UserProvider>
   )
 }
